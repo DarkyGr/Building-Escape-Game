6e728dd3935b268717bd1097a1a4b62790989ade
Guillermo Riaño <guilledark17@gmail.com>
2023-01-28 14:07:25 -0600

    ✅ Adding Tests: Rotating An Actor With Code 1

---
+++ Source/Building_Escape/OpenDoor.cpp
@@ -0,0 +1,34 @@
+// Copyright DarkyGr 2023
+
+
+#include "OpenDoor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
